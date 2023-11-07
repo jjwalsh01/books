ceb5f6d8fb3e100fcf400524af759d9e73afca74
jjwalsh01 <jjwalsh01@gmail.com>
2023-11-07 14:18:22 -0600

    ch13

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings  # new
+from django.conf.urls.static import static  # new
 from django.contrib import admin
 from django.urls import path, include
 
@@ -8,5 +10,7 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     # Local apps
     path("", include("pages.urls")),
-    path("books/", include("books.urls")),  # new
-]
+    path("books/", include("books.urls")),
+] + static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)  # new
